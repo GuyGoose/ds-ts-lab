b5afe7b044d3f369223bba358ce3ec6ec131f5f9
GuyGoose <antokeeffe@outlook.ie>
2023-09-18 11:44:59 +0100

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -4,6 +4,15 @@ interface Friend {
     age: number
 }
 
+interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+        email: string;
+        extension: number
+    }
+}
+
 const friend1 = {
     name: "Cian",
     phone: "087-12345",
